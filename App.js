d8f5b40c8efda646da1315013b47119486b8fa24
chris097 <chrisfidel.international@gmail.com>
2023-03-16 08:52:26 +0100

    add and remove item

--- App.js
+++ App.js
@@ -1,15 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  FlatList
+} from 'react-native';
+import GoalInput from './components/GoalInput';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
+
+  const [courseGoals, setCourseGoals] = useState([]);
+  
+  const addGoalHandler = (enteredGoalText) => {
+    setCourseGoals(currentCourseGoals => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, id: Math.random().toString() }
+    ]);
+  };
+
+  const handleDeleteItem = (id) => {
+    setCourseGoals(currentCourseGoals => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    })
+  }
+
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal...' />
-        <Button title="Add Goal" />
-      </View>
-      <View>
-        <Text>List of goals.</Text>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <View style={styles.goalContainer}>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return <GoalItem
+              text={itemData.item.text}
+              id={itemData.item.id}
+              onDeleteItem={handleDeleteItem}
+            />;
+          }}
+          keyExtractor={(item, index) => {
+            return item.id
+          }}
+          alwaysBounceVertical={false}
+        />
       </View>
     </View>
   );
@@ -17,17 +49,11 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  goalContainer: {
+    flex: 5,
   },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '80%',
-    marginRight: 8,
-    padding: 8
-  }
 });
